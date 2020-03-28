edfc578f10c756bde805a06f4ab28778d560b197
Justin Hsu <justin.hsu@gtri.gatech.edu>
2020-03-28 19:40:49 -0400

    More code cleanup.

--- app/src/main/java/org/coepi/android/ui/container/TabsContainerFragment.kt
+++ app/src/main/java/org/coepi/android/ui/container/TabsContainerFragment.kt
@@ -5,13 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
-//import org.coepi.android.R.id.bottom_navigation
-import org.coepi.android.R.id.navHostFragment
 import org.coepi.android.databinding.FragmentTabsContainerBinding.inflate
-import org.coepi.android.ui.onboarding.OnboardingViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class TabsContainerFragment : Fragment() {
