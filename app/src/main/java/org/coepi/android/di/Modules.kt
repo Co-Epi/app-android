0a7a79e504bd1b1e8ce72737ed6117a4d48b8abc
Bartolomeo <bartolomeo@bart-desktop.com>
2020-04-01 00:35:37 +0200

    added 1 space

--- app/src/main/java/org/coepi/android/di/Modules.kt
+++ app/src/main/java/org/coepi/android/di/Modules.kt
@@ -25,7 +25,7 @@ import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModel { HomeViewModel(get()) }
-    viewModel { SymptomsViewModel(get(),get()) }
+    viewModel { SymptomsViewModel(get(), get()) }
     viewModel { SettingsViewModel() }
     viewModel { CENViewModel(get(), get(), get()) }
     viewModel { LocationViewModel() }
