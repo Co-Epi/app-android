8a174ea8c8b3a525b2a8240dd8e0fe84d75c78d2
Bartolomeo <bartolomeo@bart-desktop.com>
2020-04-01 01:01:23 +0200

    inject repo instead RealmProvier

--- app/src/main/java/org/coepi/android/ui/symptoms/SymptomsViewModel.kt
+++ app/src/main/java/org/coepi/android/ui/symptoms/SymptomsViewModel.kt
@@ -30,7 +30,7 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.Date
 
-class SymptomsViewModel(private val symptomRepo: SymptomRepo, private val realmprovider: RealmProvider) : ViewModel() {
+class SymptomsViewModel(private val symptomRepo: SymptomRepo, private val repo: CenRepo ) : ViewModel() {
 
     private val selectedSymptomIds: BehaviorSubject<Set<String>> =
         BehaviorSubject.createDefault(emptySet())
@@ -75,27 +75,13 @@ class SymptomsViewModel(private val symptomRepo: SymptomRepo, private val realmp
     }
     fun submitSymptoms(){
         log.i("About to send sypmtom:");
-        if( realmprovider!= null ){
-            val keys: String = "will be put by doPostSymptom using last 3 keys generated"
-            val report = "COVID 19 confirmed"
-            val id = 1
-            val symptomReport = SymptomReport(id, report,keys)
-            val retrofit = Retrofit.Builder()
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .addConverterFactory(GsonConverterFactory.create())
-                .baseUrl("https://coepi.wolk.com:8080")
-                .build()
-            val api = retrofit.create(CENApi::class.java)
+        val keys: String = "will be put by doPostSymptom using last 3 keys generated"
+        val report = "COVID 19 confirmed"
+        val id = 1
+        val symptomReport = SymptomReport(id, report,keys)
 
-            val cenDao = RealmCenDao(realmprovider)
-            val cenkeyDao =  RealmCenKeyDao(realmprovider)
-            val cenReportDao= RealmCenReportDao(realmprovider)
-            val repo = CenRepo(api, cenDao, cenkeyDao, cenReportDao)
-            repo.doPostSymptoms( symptomReport )
-            log.i("Reported: {reportID:\""+symptomReport.reportID+"\", report:\""+symptomReport.report+"\", cenkeys: \""+ symptomReport.cenKeys+"\"}" )
-        }else{
-            log.i("Could not get context in init")
-        }
+        repo.doPostSymptoms( symptomReport )
+        log.i("Reported: {reportID:\""+symptomReport.reportID+"\", report:\""+symptomReport.report+"\", cenkeys: \""+ symptomReport.cenKeys+"\"}" )
 
     }
 
