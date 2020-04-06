a623609848283344f22de258244b1c73e5b51307
Justin Hsu <justin.hsu@gtri.gatech.edu>
2020-04-06 15:53:37 -0400

    Formatting and removed unused imports.

--- app/src/main/java/org/coepi/android/ui/onboarding/OnboardingFragment.kt
+++ app/src/main/java/org/coepi/android/ui/onboarding/OnboardingFragment.kt
@@ -1,12 +1,10 @@
 package org.coepi.android.ui.onboarding
 
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import org.coepi.android.R
 import org.coepi.android.databinding.FragmentOnboardingBinding.inflate
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
