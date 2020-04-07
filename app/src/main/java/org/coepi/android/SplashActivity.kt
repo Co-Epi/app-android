f31f42a1f10827e63d872b07f035b33ae8c86f5e
Justin Hsu <justin.hsu@gtri.gatech.edu>
2020-04-07 13:45:22 -0400

    Added splash screen.

---
+++ app/src/main/java/org/coepi/android/SplashActivity.kt
@@ -0,0 +1,16 @@
+package org.coepi.android
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
