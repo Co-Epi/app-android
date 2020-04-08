deadec0d03cd63b9bc98bcc788e648af9b50b02f
Ivan Schuetz <ivanschuetz@gmail.com>
2020-04-08 17:02:13 +0200

    Send keys as array, remove rejected fields

--- app/src/main/java/org/coepi/android/api/request/ApiParamsCenReport.kt
+++ app/src/main/java/org/coepi/android/api/request/ApiParamsCenReport.kt
@@ -2,22 +2,15 @@ package org.coepi.android.api.request
 
 import org.coepi.android.cen.CenKey
 import org.coepi.android.cen.SymptomReport
-import org.coepi.android.extensions.coEpiTimestamp
 import org.coepi.android.extensions.toBase64
-import org.coepi.android.extensions.toHex
-import java.util.Date
 
 data class ApiParamsCenReport(
-    var reportID: String,
     var report: String,
-    var cenKeys: List<String>,
-    var reportTimeStamp: Long
+    var cenKeys: List<String>
 )
 
 fun SymptomReport.toApiParamsCenReport(keys: List<CenKey>) =
     ApiParamsCenReport(
-        reportID = id.toByteArray().toHex(),
         report = report.toBase64(),
-        cenKeys = keys.map { it.key }, //this has to be HEX
-        reportTimeStamp = Date().coEpiTimestamp()
+        cenKeys = keys.map { it.key } //this has to be HEX
     )
