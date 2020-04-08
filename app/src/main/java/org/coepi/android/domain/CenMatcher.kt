09471f7f1b13bb233095e8f455cb7ec62447c5ff
Bartolomeo <bartolomeo@bart-desktop.com>
2020-04-08 05:48:18 +0200

    matching works

--- app/src/main/java/org/coepi/android/domain/CenMatcher.kt
+++ app/src/main/java/org/coepi/android/domain/CenMatcher.kt
@@ -3,6 +3,8 @@ package org.coepi.android.domain
 import org.coepi.android.cen.CenKey
 import org.coepi.android.cen.RealmCenDao
 import org.coepi.android.cen.RealmReceivedCen
+import org.coepi.android.extensions.coEpiTimestamp
+import java.util.Date
 
 interface CenMatcher {
     fun hasMatches(key: CenKey, maxTimestamp: Long): Boolean
@@ -25,16 +27,17 @@ class CenMatcherImpl(
 
         // take the last 7 days of timestamps and generate all the possible CENs (e.g. 7 days) TODO: Parallelize this?
 
-        val minTimestamp = maxTimestamp - secondsInAWeek
+        val maxRealTimeStamp = Date().coEpiTimestamp()
+        val minTimestamp = maxRealTimeStamp - secondsInAWeek
         val possibleCensCount = (secondsInAWeek / cenLifetimeInSeconds).toInt()
 
         val possibleCENs = Array(possibleCensCount) { i ->
-            val ts = maxTimestamp - cenLifetimeInSeconds * i
+            val ts = maxRealTimeStamp - cenLifetimeInSeconds * i
             val cen = cenLogic.generateCen(key, ts)
             cen.toHex()
         }
 
         // check if the possibleCENs are in the CEN Table
-        return cenDao.matchCENs(minTimestamp, maxTimestamp, possibleCENs)
+        return cenDao.matchCENs(minTimestamp, maxRealTimeStamp, possibleCENs)
     }
 }
