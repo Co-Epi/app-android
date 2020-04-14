92b43a8abc03bf8443d7441bf73cd2b0d7e49444
Ivan Schütz <ivanschuetz@gmail.com>
2020-04-14 22:20:42 +0200

    Fetch keys only since last fetch (#86)

--- app/src/main/java/org/coepi/android/worker/cenfetcher/ContactsFetchWorker.kt
+++ app/src/main/java/org/coepi/android/worker/cenfetcher/ContactsFetchWorker.kt
@@ -8,8 +8,15 @@ import androidx.work.workDataOf
 import kotlinx.coroutines.delay
 import org.coepi.android.cen.RealmCenReportDao
 import org.coepi.android.cen.ReceivedCenReport
+import org.coepi.android.common.doIfSuccess
 import org.coepi.android.common.successOrNull
+import org.coepi.android.domain.CoEpiDate
+import org.coepi.android.domain.CoEpiDate.Companion.fromUnixTime
+import org.coepi.android.domain.CoEpiDate.Companion.minDate
+import org.coepi.android.domain.CoEpiDate.Companion.now
 import org.coepi.android.repo.CoEpiRepo
+import org.coepi.android.system.Preferences
+import org.coepi.android.system.PreferencesKey.LAST_CEN_KEYS_FETCH_TIMESTAMP
 import org.coepi.android.system.log.LogTag.CEN_MATCHING
 import org.coepi.android.system.log.log
 import org.koin.core.KoinComponent
@@ -22,15 +29,28 @@ class ContactsFetchWorker(
 
     private val coEpiRepo: CoEpiRepo by inject()
     private val reportsDao: RealmCenReportDao by inject()
+    private val preferences: Preferences by inject()
 
     override suspend fun doWork(): Result {
         log.d("Contacts fetch worker started...", CEN_MATCHING)
 
-        val reportsResult = coEpiRepo.reports()
+        val fromDate: CoEpiDate = preferences.getLong(LAST_CEN_KEYS_FETCH_TIMESTAMP)?.let {
+            fromUnixTime(it)
+        } ?: minDate()
+
+        val nowBeforeRequest: CoEpiDate = now()
+
+        val reportsResult =
+            coEpiRepo.reports(fromDate)
         val reports: List<ReceivedCenReport> = reportsResult.successOrNull() ?: emptyList()
 
         reports.forEach {
             reportsDao.insert(it.report)
+        }
+
+        // After reports saved successfully, store the fetch timestamp
+        reportsResult.doIfSuccess {
+            preferences.putLong(LAST_CEN_KEYS_FETCH_TIMESTAMP, nowBeforeRequest.unixTime)
         }
 
         setProgress(workDataOf(CONTACT_COUNT_KEY to reports.size))
@@ -39,6 +59,7 @@ class ContactsFetchWorker(
         delay(100)
 
         log.i("Contacts fetch worker finished. Saved reports: ${reports.size}", CEN_MATCHING)
+
         return success()
     }
 
