32849b6693de8426a670f3ceaa74e23d1677d157
Ivan Schütz <ivanschuetz@gmail.com>
2020-04-22 13:50:01 +0200

    Reset Realm when it needs migration (#102)

--- app/src/main/java/org/coepi/android/repo/RealmProvider.kt
+++ app/src/main/java/org/coepi/android/repo/RealmProvider.kt
@@ -3,11 +3,18 @@ package org.coepi.android.repo
 import android.content.Context
 import io.realm.Realm
 import io.realm.Realm.getDefaultInstance
+import io.realm.RealmConfiguration
+import io.realm.RealmConfiguration.Builder
 
-class RealmProvider(private val applicationContext: Context) {
+class RealmProvider(applicationContext: Context) {
     init {
         Realm.init(applicationContext)
+        Realm.setDefaultConfiguration(defaultConfig())
     }
 
     val realm: Realm get() = getDefaultInstance()
+
+    private fun defaultConfig(): RealmConfiguration = Builder()
+        .deleteRealmIfMigrationNeeded() // Useful during early development: Reset data if the schema changes
+        .build()
 }
