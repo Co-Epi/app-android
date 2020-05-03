97dda03f39f8111257d5911a3a11feadaba24a43
Ivan Schuetz <ivanschuetz@gmail.com>
2020-05-03 18:45:39 +0200

    Adapt BLE simulator to 0.4 reports

--- app/src/main/java/org/coepi/android/ble/BleSimulator.kt
+++ app/src/main/java/org/coepi/android/ble/BleSimulator.kt
@@ -3,42 +3,31 @@ package org.coepi.android.ble
 import io.reactivex.Observable
 import io.reactivex.Observable.fromIterable
 import org.coepi.android.cen.Cen
-import org.coepi.android.cen.CenKey
-import org.coepi.android.domain.CenLogic
-import org.coepi.android.domain.UnixTime.Companion.now
+import org.coepi.android.extensions.base64ToByteArray
 import org.coepi.android.system.log.log
+import org.tcncoalition.tcnclient.crypto.SignedReport
 
-class BleSimulator(cenLogic: CenLogic) : BleManager {
+class BleSimulator : BleManager {
 
-    // Keys to test locally
-    private val keys: List<CenKey> = listOf(
-        CenKey("F2BA29936ED6898157CD839FE50ACF40998533C3A4FECFD2B9FA252E0B10E14B", now()),
-        CenKey("e7c63d828922422cbba7ffed3f858598d5c97ec34442ec875b74cffdf316edd6", now())
+    // Reports used to derive fake observed TCNs from
+    private val reports: List<String> = listOf(
+//        "LbSUvv320gtY2qTZbumxno7KJ/BDWnHuHcUH0fNv144p+K1xbPt+YQuxFHzFfo71HoegSspNJLaAz93InuQHHQEAAQAACFJtVjJaWEk9iXj1FGy+r4cmNrS84AzHzx5wS0FZJXzXFFfvqwAogt6qjIe7+6CIJ8mrFrCen3nAVrQo3Bd1jsGe6UjybRUlAA=="
+        "rSqWpM3ZQm7hfQ3q2x2llnFHiNhyRrUQPKEtJ33VKQcwT7Ly6e4KGaj5ZzjWt0m4c0v5n/VH5HO9UXbPXvsQTgEAQQAALFVtMVdNbHBZU1hOSlJYaDJZek5OWjJJeVdXZFpXRUozV2xoU2NHUkhWVDA9jn0pZAeME6ZBRHJOlfIikyfS0Pjg6l0txhhz6hz4exTxv8ryA3/Z26OebSRwzRfRgLdWBfohaOwOcSaynKqVCg=="
     )
 
-    private val cens: List<Cen> = keys.map {
-        cenLogic.generateCen(it, now().value)
+    private val cens: List<Cen> = reports.mapNotNull { report ->
+        val signedReport = report.base64ToByteArray()?.let { SignedReport.fromByteArray(it) }
+        signedReport?.report?.temporaryContactNumbers?.let {
+            if (it.hasNext()) { it.next() } else { null }
+        }?.let { Cen(it.bytes) }
     }
 
     // Emits all the cens at once and terminates
     override val observedCens: Observable<Cen> = fromIterable(cens)
 
-    // Use this to emit periodically
-//    private var currentCenIndex = 0
-//    override val observedCens: Observable<Cen> =
-//        Observable.interval(0, 5, SECONDS)
-//            .subscribeOn(Schedulers.io())
-//            .observeOn(AndroidSchedulers.mainThread())
-//            .flatMap {
-//                if (cens.isEmpty()) {
-//                    empty()
-//                } else {
-//                    just(cens[currentCenIndex % cens.size])
-//                }
-//            }
-
     init {
         log.i("Using Bluetooth simulator")
+        log.i("Bluetooth cens: $cens")
     }
 
     override fun startService() {}
