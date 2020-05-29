d56377a66928bac0462c8a84a28f6b794014ca79
Cole <43547244+Joshua-McCord@users.noreply.github.com>
2020-05-29 14:24:23 -0400

    Ux update exposure details (#185)
    * Alert Detail Header showing Date
    
    * Remove the Date of Symptoms
    
    * Code Cleanup
    
    * Addressed Comments
    
    * Addressing Comments about ViewModel and DateFormatters

--- app/src/main/java/org/coepi/android/ui/alertsdetails/AlertsDetailsViewModel.kt
+++ app/src/main/java/org/coepi/android/ui/alertsdetails/AlertsDetailsViewModel.kt
@@ -4,9 +4,11 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import io.reactivex.Observable.just
 import org.coepi.android.R.drawable.ic_alert
+import org.coepi.android.domain.UnixTime
 import org.coepi.android.tcn.Alert
 import org.coepi.android.domain.symptomflow.SymptomId
 import org.coepi.android.extensions.rx.toLiveData
+import org.coepi.android.ui.formatters.DateFormatters
 
 // TODO remove this screen? Appears not to be used
 class AlertsDetailsViewModel(
@@ -16,9 +18,14 @@ class AlertsDetailsViewModel(
     val report: LiveData<List<AlertDetailsSymptomViewData>> = just(args.report.toViewData())
         .toLiveData()
 
-    // FIXME temporary title to identify the report. No design yet for this screen.
-    val title: LiveData<String> = just("Report: ${args.report.id}")
+    val title: LiveData<String> = just(toMonthAndDay(args.report.contactTime))
         .toLiveData()
+
+    val exposureTime: LiveData<String> = just(toHourMinute(args.report.contactTime))
+        .toLiveData()
+
+    //TODO Show when the symptoms were reported. Maybe the earliestSymptom Date?
+    val reportedTime: LiveData<String> = just(" ").toLiveData()
 
     private fun Alert.toViewData(): List<AlertDetailsSymptomViewData> =
         emptyList()
@@ -27,4 +34,12 @@ class AlertsDetailsViewModel(
     private fun SymptomId.toViewData(): AlertDetailsSymptomViewData =
         // TODO map to localized names
         AlertDetailsSymptomViewData(ic_alert, name, this)
+
+    private fun toMonthAndDay(time: UnixTime ): String =
+        DateFormatters.monthDayFormatter.formatMonthDay(time.toDate())
+
+    private fun toHourMinute(time: UnixTime): String =
+        DateFormatters.hourMinuteFormatter.formatTime(time.toDate())
 }
+
+
