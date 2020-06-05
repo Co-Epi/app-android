ef543f3164bfee7264108bb84eb2780517274677
Katrina Bedsole <katrinabedsole@gmail.com>
2020-06-05 07:38:27 -0400

    clean up SwipeToDeleteCallback class

--- app/src/main/java/org/coepi/android/ui/common/SwipeToDeleteCallback.kt
+++ app/src/main/java/org/coepi/android/ui/common/SwipeToDeleteCallback.kt
@@ -2,11 +2,9 @@ package org.coepi.android.ui.common
 
 import android.content.Context
 import android.graphics.Canvas
-import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.PorterDuff
 import android.graphics.PorterDuffXfermode
-import android.graphics.drawable.ColorDrawable
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
@@ -15,10 +13,7 @@ import androidx.recyclerview.widget.RecyclerView.ViewHolder
 abstract class SwipeToDeleteCallback(context: Context) :
     ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
 
-    private val background = ColorDrawable()
-    private val backgroundColor = Color.parseColor("#f44336")
     private val clearPaint = Paint().apply { xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR) }
-
 
     override fun getMovementFlags(recyclerView: RecyclerView, viewHolder: ViewHolder): Int {
         /**
@@ -64,15 +59,6 @@ abstract class SwipeToDeleteCallback(context: Context) :
             return
         }
 
-        // Draw the red delete background
-        background.color = backgroundColor
-        background.setBounds(
-            itemView.right + dX.toInt(),
-            itemView.top,
-            itemView.right,
-            itemView.bottom
-        )
-        background.draw(c)
         super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
     }
 
