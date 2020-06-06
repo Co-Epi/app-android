cb38e18639fd0a3f46a3e818cf064f3baac60437
Ivan Schuetz <ivanschuetz@gmail.com>
2020-06-06 12:36:23 +0200

    Show time in logs, refactor

--- app/src/main/java/org/coepi/android/ui/debug/logs/LogsAdapter.kt
+++ app/src/main/java/org/coepi/android/ui/debug/logs/LogsAdapter.kt
@@ -1,11 +1,6 @@
 package org.coepi.android.ui.debug.logs
 
 import android.annotation.SuppressLint
-import android.graphics.Color.BLACK
-import android.graphics.Color.BLUE
-import android.graphics.Color.RED
-import android.graphics.Color.YELLOW
-import android.graphics.Color.parseColor
 import android.view.HapticFeedbackConstants.LONG_PRESS
 import android.view.LayoutInflater.from
 import android.view.ViewGroup
@@ -14,17 +9,10 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import org.coepi.android.databinding.ItemLogEntryBinding
 import org.coepi.android.databinding.ItemLogEntryBinding.inflate
-import org.coepi.android.system.log.LogLevel
-import org.coepi.android.system.log.LogLevel.D
-import org.coepi.android.system.log.LogLevel.E
-import org.coepi.android.system.log.LogLevel.I
-import org.coepi.android.system.log.LogLevel.V
-import org.coepi.android.system.log.LogLevel.W
-import org.coepi.android.system.log.LogMessage
 import org.coepi.android.ui.debug.logs.LogsRecyclerViewAdapter.ViewHolder
 
 class LogsRecyclerViewAdapter(private val onItemLongClick: () -> Unit)
-    : ListAdapter<LogMessage, ViewHolder>(LogsDiffCallback()) {
+    : ListAdapter<LogMessageViewData, ViewHolder>(LogsDiffCallback()) {
 
     class ViewHolder(
         parent: ViewGroup,
@@ -32,24 +20,16 @@ class LogsRecyclerViewAdapter(private val onItemLongClick: () -> Unit)
             inflate(from(parent.context), parent, false)
     ) : RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(item: LogMessage, onItemClick: () -> Unit): Unit = binding.run {
+        fun bind(item: LogMessageViewData, onItemClick: () -> Unit): Unit = binding.run {
             this.item = item
-            this.lblLogLevel.setTextColor(item.level.color())
-            this.lblMessage.setTextColor(item.level.color())
+            time.setTextColor(item.textColor)
+            message.setTextColor(item.textColor)
 
             root.setOnLongClickListener {
                 root.performHapticFeedback(LONG_PRESS)
                 onItemClick()
                 true
             }
-        }
-
-        private fun LogLevel.color(): Int = when (this) {
-            V -> BLACK
-            D -> parseColor("#228C22")
-            I -> BLUE
-            W -> YELLOW
-            E -> RED
         }
     }
 
@@ -61,12 +41,14 @@ class LogsRecyclerViewAdapter(private val onItemLongClick: () -> Unit)
     }
 }
 
-private class LogsDiffCallback : ItemCallback<LogMessage>() {
-    override fun areItemsTheSame(oldItem: LogMessage, newItem: LogMessage): Boolean =
+private class LogsDiffCallback : ItemCallback<LogMessageViewData>() {
+    override fun areItemsTheSame(oldItem: LogMessageViewData, newItem: LogMessageViewData)
+            : Boolean =
         oldItem === newItem
 
     @SuppressLint("DiffUtilEquals")
     // 2 messages with the same content should be handled as different, so identity based
-    override fun areContentsTheSame(oldItem: LogMessage, newItem: LogMessage): Boolean =
+    override fun areContentsTheSame(oldItem: LogMessageViewData, newItem: LogMessageViewData)
+            : Boolean =
         oldItem === newItem
 }
