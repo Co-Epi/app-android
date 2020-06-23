9115b01a30cb3c1d6e11b84f34d30cce651987ce
Ivan Schuetz <ivanschuetz@gmail.com>
2020-06-23 16:56:51 +0200

    Remove outdated unit tests

--- app/src/test/java/org/coepi/android/api/memo/mapper/MapperTestUtils.kt
+++
@@ -1,7 +0,0 @@
-package org.coepi.android.api.memo.mapper
-
-object MapperTestUtils {
-
-    fun bits(count: Int, value: Boolean = false): MutableList<Boolean> = List(count) { value }
-        .toMutableList()
-}
