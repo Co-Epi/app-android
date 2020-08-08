4277152649d4b8dbf9ca3a93becc1db419875ac4
Ivan Schuetz <ivanschuetz@gmail.com>
2020-08-08 11:57:16 +0200

    Add onboarding images

--- app/src/main/java/org/coepi/android/ui/onboarding/OnboardingCardViewData.kt
+++ app/src/main/java/org/coepi/android/ui/onboarding/OnboardingCardViewData.kt
@@ -1,7 +1,10 @@
 package org.coepi.android.ui.onboarding
 
+import androidx.annotation.DrawableRes
+
 sealed class OnboardingCardViewData(val title: String, val message: CharSequence) {
-    class SmallCard(title: String, message: CharSequence, val highlightedDot: Int)
+    class SmallCard(title: String, message: CharSequence, val highlightedDot: Int,
+                    @DrawableRes val image: Int)
         : OnboardingCardViewData(title, message)
 
     class LargeCard(title: String, message: CharSequence)
