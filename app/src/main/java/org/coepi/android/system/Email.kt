54cffa441d27d18ba33d7719a34dc9b5c9125262
Dusko Ojdanic <dusko.ojdanic@gmail.com>
2020-11-22 14:55:02 +0100

    Email attachment (#356)
    * email.open - using default function parameter values
    * email db as attachment

--- app/src/main/java/org/coepi/android/system/Email.kt
+++ app/src/main/java/org/coepi/android/system/Email.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import android.content.Intent.ACTION_SEND
 import android.content.Intent.EXTRA_EMAIL
+import android.content.Intent.EXTRA_STREAM
 import android.content.Intent.EXTRA_SUBJECT
 import android.content.Intent.EXTRA_TEXT
 import android.content.Intent.createChooser
@@ -11,17 +12,18 @@ import android.net.Uri
 import org.coepi.android.system.log.log
 
 interface Email {
-    fun open(activity: Activity, recipient: String, subject: String, message: String = "")
+    fun open(activity: Activity, recipient: String, subject: String, attachmentUri: Uri? = null, message: String = "")
 }
 
 class EmailImpl: Email {
-    override fun open(activity: Activity, recipient: String, subject: String, message: String) {
+    override fun open(activity: Activity, recipient: String, subject: String, attachmentUri: Uri?, message: String) {
         val intent = Intent(ACTION_SEND).apply {
             data = Uri.parse("mailto:")
             type = "message/rfc822"
             putExtra(EXTRA_EMAIL, arrayOf(recipient))
             putExtra(EXTRA_SUBJECT, subject)
             putExtra(EXTRA_TEXT, message)
+            putExtra(EXTRA_STREAM, attachmentUri)
         }
         try {
             activity.startActivity(createChooser(intent, "Choose Email Client..."))
--- app/src/main/java/org/coepi/android/ui/settings/UserSettingsFragment.kt
+++ app/src/main/java/org/coepi/android/ui/settings/UserSettingsFragment.kt
@@ -1,9 +1,11 @@
 package org.coepi.android.ui.settings
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView.VERTICAL
@@ -11,8 +13,12 @@ import org.coepi.android.databinding.FragmentUserSettingsBinding.inflate
 import org.coepi.android.extensions.observeWith
 import org.coepi.android.system.log.log
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.io.File
 
 class UserSettingsFragment : Fragment() {
+
+    private val coepiFileProviderAuthority = "org.coepi.provider"
+    private val coepiDbFilePath = "databases/db.sqlite"
     private val viewModel by viewModel<UserSettingsViewModel>()
 
     override fun onCreateView(
@@ -22,13 +28,15 @@ class UserSettingsFragment : Fragment() {
         lifecycleOwner = viewLifecycleOwner
         vm = viewModel
 
+        val dbFileUri = getDbFileUri(coepiDbFilePath)
+
         val adapter = UserSettingsAdapter(
             onToggle = { item, toggled ->
                 viewModel.onToggle(item, toggled)
             },
             onClick = { item ->
                 activity?.let { activity ->
-                    viewModel.onClick(item, activity)
+                    viewModel.onClick(item, activity, dbFileUri)
                 } ?: {
                     log.w("No activity set clicking on setting")
                 }()
@@ -44,4 +52,18 @@ class UserSettingsFragment : Fragment() {
             this.adapter = adapter
         }
     }.root
+
+    fun getDbFileUri(filenameSuffix: String): Uri? {
+        return context?.let {
+            val file: File? = File(it.filesDir?.parent, filenameSuffix)
+            file?.let { file1 ->
+                FileProvider.getUriForFile(
+                    it,
+                    coepiFileProviderAuthority,
+                    file1
+                )
+            }
+        }
+    }
+
 }
